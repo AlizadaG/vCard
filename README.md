ad72d7c47f2bcee3d90573259e2e9b0514bfaa2a
AlizadaG <elizadegozel@gmail.com>
2024-07-04 19:26:59 +0400

    vCard

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# vCard
+# vCard Oluşturma
+
+**Proje açıklaması:**
+İlk önce kullanıcıdan kaç adet user istediği soruldu, girdiği değer sayı olmazsa hata yaşanmaması için `try-catch` kullanıldı. `HttpClient` için de yine `try-catch` kullanılarak önlem alındı. Daha sonra Kullanıcının girdiği değerin 0 ile 2000(opsiyonel olarak dahil edildi api request zamani sorun yaşanmasın diye) arasinda olup-olmadığı kontrol edildi eğer deyilse, hata mesajı geri döndürüldü.
+
+**Sonraki Adımlar:**
+- RandomUser.me API'sinden  `HttpClient` kullanarak API'den veriler alındı
+- Alınan veriler VCard sınıfına deserialize edildi
+- Daha sonra aldığımız değerlerle `vCard` oluşturulması için wikipediadan card oluşturulma kodları kullanıldı
+- Dosyayı kaydetmek için dosya yolu belirlendi
+- Oluşturulan vCard'ı `.vcf` ve `.txt` olarak kaydedildi.
---
+++ VCard/VCard/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using VCard.Models;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Olusturmak istediginiz vCard adet sayısını giriniz: ");
+        try
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+            using HttpClient client = new HttpClient();
+            try
+            {
+                if (count > 2000 || count <= 0) {
+                    Console.WriteLine("Girdiginiz sayi 0 ile 2000 arasinda olmak zorunda!!!");
+                }
+                else
+                {
+                    HttpResponseMessage response = await client.GetAsync($"https://randomuser.me/api/?results={count}");
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    };
+
+                    var resultData = JsonSerializer.Deserialize<Result>(responseBody, options);
+                    string parentDirectory = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\.."));
+                    string vcfPath = Path.Combine(parentDirectory, "vCards(vcf)"),
+                        txtPath = Path.Combine(parentDirectory, "vCards(txt)");
+
+                    foreach (var user in resultData.Results)
+                    {
+                        StringBuilder card = new StringBuilder();
+                        card.Append($@"BEGIN:VCARD
+                                VERSION:4.0
+                                N:{user.Name.First};{user.Name.Last};;;
+                                FN:{user.Name.First} {user.Name.Last}
+                                TEL;TYPE=work,voice;VALUE=uri:tel:{user.Phone}
+                                TEL;TYPE=home,voice;VALUE=uri:tel:{user.Phone}
+                                ADR;TYPE=WORK:;;{user.Location.Country};;{user.Location.City}
+                                EMAIL:{user.Email}
+                                REV:20080424T195243Z
+                                END:VCARD");
+
+                        string fileNameVcf = $"{user.Name.First}-{user.Name.Last}.{user.Id.Value}.vcf";
+                        string filePathVcf = Path.Combine(vcfPath, fileNameVcf);
+                        File.WriteAllTextAsync(filePathVcf, card.ToString());
+
+                        string fileNameTxt = $"{user.Name.First}-{user.Name.Last}.{user.Id.Value}.txt";
+                        string filePathTxt = Path.Combine(txtPath, fileNameTxt);
+                        File.WriteAllTextAsync(filePathTxt, card.ToString());
+
+                        Console.WriteLine($"{user.Name.First} {user.Name.Last} isimli userin vCard dosyasinin yolu :{filePathVcf}");
+                    }
+                }                
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Hata mesaji: {e.Message}");
+            }
+        }
+        catch(Exception message) {
+            Console.WriteLine("Sayıyı doğru formatda dahil ediniz. Hata mesajı:" + message);
+        }
+    }
+}
